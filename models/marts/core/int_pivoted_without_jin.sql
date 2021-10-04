d06dda4b82d1fafeaa1fa254f2ac9ddbd855a021
aburnwal@akamai.com <aburnwal@akamai.com>
2021-10-04 15:26:46 +0000

    test

---
+++ models/marts/core/int_pivoted_without_jin.sql
@@ -0,0 +1,18 @@
+with payments as (
+   select * from `dbt-project-tutorial`.`dbt_aburnwal`.`stg_payments`
+),
+
+final as (
+   select
+       order_id,
+       sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
+       sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
+       sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
+       sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount
+   from `dbt-project-tutorial`.`dbt_aburnwal`.`stg_payments`
+   group by 1
+)
+
+select * from final
+limit 500
+/* limit added automatically by dbt cloud */
